e88e978699535e9b161d2b257476676d88d2138f
Kristian Babic <kristianbabic75@gmail.com>
2021-06-25 18:03:37 -0600

    HelloWorld

---
+++ Practice/src/main/java/com/kb/AdderVersion1.java
@@ -0,0 +1,41 @@
+
+package com.kb;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author babic
+ */
+public class AdderVersion1 {
+    
+    public static void main(String[] args){ 
+ 
+    int sum = 0;
+int operand1 = 0;
+int operand2 = 0;
+
+Scanner myScanner = new Scanner(System.in);
+
+String stringOperand1 = "";
+String stringOperand2 = "";
+
+System.out.println("Please enter the first number to be added:");
+
+stringOperand1 = myScanner.nextLine();
+
+System.out.println("Please enter the second number to be added");
+
+stringOperand2 = myScanner.nextLine();
+
+operand1 = Integer.parseInt(stringOperand1);
+operand2 = Integer.parseInt(stringOperand2);
+
+
+sum = operand1 + operand2;
+
+System.out.println("Sum is: " + sum);
+
+}
+        }
+
---
+++ WindowMaster/src/main/java/com/kb/WindowMaster.java
@@ -0,0 +1,110 @@
+
+package com.kb;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author: Kristian Babic
+ * e-mail: Kristianbabic75@gmail.com
+ * date:: 24.06.2021
+ */
+public class WindowMaster {
+    public static void main(String [] args) {
+       
+// try catch trial
+        try {
+        
+        float height;
+        float width;
+        
+     //Refactor amount of windows
+        float numberOfWindows;
+        
+        
+     //Refactor manual price entry   
+        float priceWindow;
+        float priceTrim;
+        
+        
+        
+        String stringHeight;
+        String stringWidth;
+        
+         //Refactor amount of windows
+        String stringNumber;
+        
+        //Refactor manual price entry
+        String stringpriceWindow;
+        String stringpriceTrim;
+                
+        
+        float areaOfWindow;
+        float cost;
+        float perimeterOfWindow;
+        
+                
+        Scanner myScanner = new Scanner(System.in);
+        
+   
+        
+        System.out.println("Please enter window height:");
+        stringHeight = myScanner.nextLine();
+        
+        System.out.println("Please enter your window width");
+        stringWidth = myScanner.nextLine();
+        
+        //Refactor manual price entry
+        System.out.println("Enter the price of the Window");
+        stringpriceWindow = myScanner.nextLine();
+        
+        System.out.println("Enter the price of the Trim");
+        stringpriceTrim = myScanner.nextLine();
+        
+         //Refactor amount of windows
+        
+        System.out.println("Number of Windows");
+        stringNumber = myScanner.nextLine();
+        
+        
+        height = Float.parseFloat(stringHeight);
+        width = Float.parseFloat(stringWidth);
+        
+        //Refactor amount of windows
+         numberOfWindows = Float.parseFloat(stringNumber);
+        
+       //Refactor manual price entry
+       priceWindow = Float.parseFloat(stringpriceWindow);
+       priceTrim = Float.parseFloat(stringpriceTrim);
+        
+  
+          
+        areaOfWindow = height * width;
+        perimeterOfWindow = 2* (height + width);
+        
+        //Refactot Manual Price entry and Amount of Windows
+        cost = ((areaOfWindow * priceWindow) + (priceTrim * perimeterOfWindow)) * (numberOfWindows);
+        
+              
+                
+        System.out.println("Window height = " + stringHeight);
+        System.out.println("Window width = " + stringWidth);
+        System.out.println("Window area = " + areaOfWindow);
+        System.out.println("Window perimeter = " + perimeterOfWindow);
+        System.out.println("Cost = " + cost);
+        
+        // Try Catch trial
+    String input = myScanner.nextLine();
+    int number = Integer.parseInt(input);  
+  } catch(NumberFormatException ex) {
+    System.out.println("Wrong entry. Please enter numbers only");
+}
+ }
+}
+/**
+ * It works fine for the calculation and catching any words
+ * but when I hit enter after the system gives me the cost 
+ * then I get the message error Message. I understand that because enter is
+ * not a number. However I don't know how to bypass it.
+ */
+   
