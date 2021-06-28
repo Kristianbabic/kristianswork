324b933b8e48033b17bcb05d7773f07113f9e2e2
Kristian Babic <kristianbabic75@gmail.com>
2021-06-27 20:10:56 -0600

    Hello.java

---
+++ Hello.java
@@ -0,0 +1,20 @@
+
+package com.kb.helloworld;
+
+/**
+ *
+ * @author Kristian Babic
+ * e-mail: kristianbabic75@gmail.com
+ * date: 18.06.2021
+ * purpose: My first Hello World program in an IDE
+ */
+public class Hello {
+    
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, World");
+        System.out.println("My name is Kristian");
+        
+    }
+    
+}
